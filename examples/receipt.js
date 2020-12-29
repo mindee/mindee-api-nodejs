33ef7c0f6d90abb852e7ed1216ae1d874bb5197d
Aude Lejuez <aude@derniercri.io>
2020-12-29 17:11:52 +0100

    🐛 fix: Fix issue with base64 file

--- examples/receipt.js
+++ examples/receipt.js
@@ -15,18 +15,18 @@ mindeeClient.receipt
     console.error(err);
   });
 
-// parsing receipt from base64 picture (Soon implemented)
-// const fs = require("fs");
-// const base64 = fs.readFileSync("./documents/receipts/receipt.jpg", {
-//   encoding: "base64",
-// });
-// mindeeClient.receipt
-//   .parse(base64, "base64")
-//   .then((res) => {
-//     console.log("Success !");
-//     console.log(res.receipts);
-//     console.log(res.receipt);
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
+// parsing receipt from base64 picture
+const fs = require("fs");
+const base64 = fs.readFileSync("./documents/receipts/receipt.jpg", {
+  encoding: "base64",
+});
+mindeeClient.receipt
+  .parse(base64, "base64")
+  .then((res) => {
+    console.log("Success !");
+    console.log(res.receipts);
+    console.log(res.receipt);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
