3faeb708cd2649e6c182eca4e658d4150618a9c9
Ianaré Sévi <ianare@mindee.co>
2022-08-04 16:22:42 +0200

    add custom API classifications

--- src/documents/custom.ts
+++ src/documents/custom.ts
@@ -1,12 +1,14 @@
 import { Document, DocumentConstructorProps } from "./document";
-import { ListField } from "../fields";
+import { ClassificationField, ListField } from "../fields";
+import { stringDict } from "../fields/field";
 
 export interface CustomDocConstructorProps extends DocumentConstructorProps {
   documentType: string;
 }
 
 export class CustomDocument extends Document {
-  fields: { [key: string]: ListField };
+  fields: Map<string, ListField> = new Map();
+  classifications: Map<string, ClassificationField> = new Map();
 
   constructor({
     inputFile,
@@ -15,22 +17,51 @@ export class CustomDocument extends Document {
     documentType,
   }: CustomDocConstructorProps) {
     super(documentType, inputFile, pageId);
-    this.fields = {};
 
     Object.keys(apiPrediction).forEach((fieldName) => {
-      this.fields[fieldName] = new ListField({
-        prediction: apiPrediction[fieldName],
-        pageId: pageId,
-      });
+      this.setField(fieldName, apiPrediction, pageId);
     });
+  }
+
+  protected setField(
+    fieldName: string,
+    apiPrediction: stringDict,
+    pageId: number | undefined
+  ) {
+    // Currently, two types of fields possible in a custom API response:
+    // fields having a list of values, and classification fields.
+
+    const fieldPrediction: stringDict = apiPrediction[fieldName];
+
+    if (fieldPrediction["values"] !== undefined) {
+      // Only value lists have the 'values' attribute.
+      this.fields.set(
+        fieldName,
+        new ListField({
+          prediction: fieldPrediction,
+          pageId: pageId,
+        })
+      );
+    } else if (fieldPrediction["value"] !== undefined) {
+      // Only classifications have the 'value' attribute.
+      this.classifications.set(
+        fieldName,
+        new ClassificationField({ prediction: fieldPrediction })
+      );
+    } else {
+      throw "Unknown API field type";
+    }
   }
 
   toString(): string {
     let outStr = `----- ${this.internalDocType} -----`;
     outStr += `\nFilename: ${this.filename}`.trimEnd();
-    for (const [name, fieldData] of Object.entries(this.fields)) {
+    this.classifications.forEach((fieldData, name) => {
       outStr += `\n${name}: ${fieldData}`.trimEnd();
-    }
+    });
+    this.fields.forEach((fieldData, name) => {
+      outStr += `\n${name}: ${fieldData}`.trimEnd();
+    });
     outStr += "\n----------------------\n";
     return outStr;
   }
