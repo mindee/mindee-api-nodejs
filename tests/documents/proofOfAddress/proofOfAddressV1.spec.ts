dd50b71659a2ba37176f3b904937b7523f88f86d
Kvin <kevin.blenner@mindee.co>
2023-01-24 11:42:08 +0100

    test

--- tests/documents/proofOfAddress/proofOfAddressV1.spec.ts
+++ tests/documents/proofOfAddress/proofOfAddressV1.spec.ts
@@ -5,7 +5,22 @@ import { dataPath } from "../../apiPaths";
 import { ProofOfAddressV1 } from "../../../src/documents";
 
 describe("Proof of Address V1 Object initialization", async () => {
-  it.only("should load a complete document prediction", async () => {
+  it("should load an empty document prediction", async () => {
+    const jsonDataNA = await fs.readFile(
+      path.resolve(dataPath.proofOfAddressV1.empty)
+    );
+    const response = JSON.parse(jsonDataNA.toString());
+    const doc = new ProofOfAddressV1({
+      prediction: response.document.inference.prediction,
+    });
+    expect(doc.recipientName.value).to.be.undefined;
+    expect(doc.recipientAddress.value).to.be.undefined;
+    expect(doc.recipientCompanyRegistrations.length).to.be.equals(0);
+    expect(doc.issuerName.value).to.be.undefined;
+    expect(doc.issuerAddress.value).to.be.undefined;
+    expect(doc.issuerCompanyRegistrations.length).to.be.equals(0);
+  });
+  it("should load a complete document prediction", async () => {
     const jsonData = await fs.readFile(
       path.resolve(dataPath.proofOfAddressV1.complete)
     );
