c96ab32d48f12e3cce992ba872c1b1077f86ccd7
Ianaré Sévi <ianare@mindee.co>
2023-04-20 11:10:13 +0200

    :white_check_mark: minor improvements to tests

--- tests/test_code_samples.sh
+++ tests/test_code_samples.sh
@@ -13,23 +13,21 @@ cd ../test_code_samples
 npm install ../mindee-api-nodejs/dist
 cd -
 
-for f in $(find docs/code_samples -name "*.txt" -maxdepth 1 | sort -h)
+for f in $(find docs/code_samples -maxdepth 1 -name "*.txt" | sort -h)
 do
   echo "###############################################"
   echo "${f}"
   echo "###############################################"
   echo
 
-  cat "${f}" > $OUTPUT_FILE
+  sed "s/my-api-key/$API_KEY/" > $OUTPUT_FILE
+  sed -i "s/\/path\/to\/the\/file.ext/..\/mindee-api-nodejs\/tests\/data\/pdf\/blank_1.pdf/" $OUTPUT_FILE
 
   if echo "$f" | grep -q "custom_v1.txt"
   then
     sed -i "s/my-account/$ACCOUNT/g" $OUTPUT_FILE
     sed -i "s/my-endpoint/$ENDPOINT/g" $OUTPUT_FILE
   fi
-
-  sed -i "s/my-api-key/$API_KEY/" $OUTPUT_FILE
-  sed -i "s/\/path\/to\/the\/file.ext/..\/mindee-api-nodejs\/tests\/data\/pdf\/blank_1.pdf/" $OUTPUT_FILE
 
   node $OUTPUT_FILE
 done
