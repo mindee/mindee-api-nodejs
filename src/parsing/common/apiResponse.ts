0d652ae6f613109a030b31ada2f3c95a944f7564
Sebastian <130448732+sebastianMindee@users.noreply.github.com>
2023-08-28 09:30:40 +0200

    :sparkles: add raw http from requests (#206)

--- src/parsing/common/apiResponse.ts
+++ src/parsing/common/apiResponse.ts
@@ -9,12 +9,22 @@ import { StringDict } from "./stringDict";
 export abstract class ApiResponse {
   /** Initial request sent to the API. */
   apiRequest: ApiRequest;
+  /** Raw text representation of the API's response. */
+  private readonly rawHttp: StringDict;
 
   /**
-   * 
    * @param serverResponse JSON response from the server.
    */
   constructor(serverResponse: StringDict) {
     this.apiRequest = new ApiRequest(serverResponse["api_request"]);
+    this.rawHttp = serverResponse;
+  }
+
+  /**
+   * Raw HTTP request sent from server, as a JSON-like structure
+   * @returns The HTTP request
+   */
+  getRawHttp():StringDict {
+    return this.rawHttp;
   }
 }
