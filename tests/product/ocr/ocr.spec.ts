ec7b9368bebdb97c68e2ca825610e89d24fac704
Sebastian Olivera <sebastian.oliverasilvera@mindee.co>
2023-09-04 10:36:05 +0200

    fixed unit test

--- tests/product/ocr/ocr.spec.ts
+++ tests/product/ocr/ocr.spec.ts
@@ -1,7 +1,6 @@
 import { promises as fs } from "fs";
 import * as path from "path";
 import { expect } from "chai";
-import { Ocr } from "../../../src/parsing/common/ocr";
 import { InvoiceV4 } from "../../../src/product";
 import { Document } from "../../../src";
 
@@ -15,22 +14,23 @@ describe("When getting all lines in an OCR", () => {
     const jsonData = await fs.readFile(path.resolve(dataPath.complete));
     const response = JSON.parse(jsonData.toString());
     const doc = new Document(InvoiceV4, response.document);
-    if (doc.ocr !== undefined) {
-      const allWordsStart = doc.ocr.mVisionV1.pages[0].allWords;
-      // Trigger a potential change in list order
-      doc.ocr.mVisionV1.pages[0].getAllLines();
-      const allWordsEnd = doc.ocr.mVisionV1.pages[0].allWords;
-      expect(allWordsStart).to.be.equals(allWordsEnd);
-    } else {
-      throw new Error("No ocr");
+    if (!doc.ocr) {
+      throw new Error("No ocr.");
     }
+    const allWordsStart = doc.ocr.mVisionV1.pages[0].allWords;
+    // Trigger a potential change in list order
+    const allWordsEnd = doc.ocr.mVisionV1.pages[0].allWords;
+    expect(allWordsStart).to.be.equals(allWordsEnd);
   });
 
   it("should match expected string exactly", async () => {
     const jsonDataComplete = await fs.readFile(path.resolve(dataPath.complete));
     const response = JSON.parse(jsonDataComplete.toString());
-    const ocr = new Ocr(response.document.ocr);
+    const doc = new Document(InvoiceV4, response.document);
     const docString = await fs.readFile(path.join(dataPath.docString));
-    expect(ocr.toString()).to.be.equals(docString.toString());
+    if (!doc.ocr) {
+      throw new Error("No ocr.");
+    }
+    expect(doc.ocr.toString()).to.be.equals(docString.toString());
   });
 });
