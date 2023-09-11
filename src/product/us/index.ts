3b4d7bc7a803beebe4b4587f14d04d0b1ed7a8ba
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2023-09-11 18:32:04 +0200

    :sparkles: add w9

--- src/product/us/index.ts
+++ src/product/us/index.ts
@@ -1,2 +1,3 @@
 export { BankCheckV1 } from "./bankCheck/bankCheckV1";
 export { DriverLicenseV1 } from "./driverLicense/driverLicenseV1";
+export { W9V1 } from "./w9/w9V1";
---
+++ src/product/us/w9/w9V1Document.ts
@@ -0,0 +1,14 @@
+import { Prediction } from "../../../parsing/common";
+
+
+/**
+ * Document data for US W9, API version 1.
+ */
+export class W9V1Document implements Prediction {
+  /**
+   * Default string representation.
+   */
+  toString(): string {
+    return "";
+  }
+}
