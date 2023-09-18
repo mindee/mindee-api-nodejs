8af0fe3ffb7d5ff935f448f7aec53cfcbd361efb
Ianaré Sévi <ianare@mindee.co>
2023-09-18 12:17:33 +0200

    :recycle: rework line items functions

--- src/geometry/boundingBoxUtils.ts
+++ src/geometry/boundingBoxUtils.ts
@@ -6,12 +6,7 @@ import { Polygon } from "./polygon";
  */
 export function getBoundingBox(polygon: Polygon): BoundingBox {
   const bbox = getBbox(polygon);
-  return [
-    [bbox[0], bbox[1]],
-    [bbox[2], bbox[1]],
-    [bbox[2], bbox[3]],
-    [bbox[0], bbox[3]],
-  ];
+  return getBoundingBoxFromBBox(bbox);
 }
 
 /**
@@ -19,10 +14,10 @@ export function getBoundingBox(polygon: Polygon): BoundingBox {
  */
 export function getBoundingBoxFromBBox(bbox: BBox): BoundingBox {
   return [
-    [bbox[0], bbox[1]],
-    [bbox[2], bbox[1]],
-    [bbox[2], bbox[3]],
-    [bbox[0], bbox[3]],
+    [bbox.xMin, bbox.yMin],
+    [bbox.xMax, bbox.yMin],
+    [bbox.xMax, bbox.yMax],
+    [bbox.xMin, bbox.yMax],
   ];
 }
 
@@ -30,36 +25,38 @@ export function getBoundingBoxFromBBox(bbox: BBox): BoundingBox {
  * Given 2 bbox, merge them.
  */
 export function mergeBbox(bbox1: BBox, bbox2: BBox): BBox {
-  return [
-    Math.min(bbox1[0], bbox2[0]),
-    Math.min(bbox1[1], bbox2[1]),
-    Math.max(bbox1[2], bbox2[2]),
-    Math.max(bbox1[3], bbox2[3]),
-  ];
+  return new BBox(
+    Math.min(bbox1.xMin, bbox2.xMin),
+    Math.min(bbox1.yMin, bbox2.yMin),
+    Math.max(bbox1.xMax, bbox2.xMax),
+    Math.max(bbox1.yMax, bbox2.yMax),
+  );
 }
 
 /**
- * Given a Polygon, calculate a bounding box that encompasses all points.
+ * Given a Polygon, calculate a BBox that encompasses all points.
  */
 export function getBbox(polygon: Polygon): BBox {
-  const allY = polygon.map((point) => point[1]);
-  const allX = polygon.map((point) => point[0]);
-  const yMin = Math.min(...allY);
-  const yMax = Math.max(...allY);
-  const xMin = Math.min(...allX);
-  const xMax = Math.max(...allX);
-  return [xMin, yMin, xMax, yMax];
+  const allY: number[] = polygon.map((point) => point[1]);
+  const allX: number[] = polygon.map((point) => point[0]);
+  return new BBox(
+    Math.min(...allX),
+    Math.min(...allY),
+    Math.max(...allX),
+    Math.max(...allY),
+  );
 }
 
 /**
- * Given polygons, calculate a bounding box that encompasses all points.
+ * Given polygons, calculate a BBox that encompasses all points.
  */
 export function getBBoxForPolygons(polygons: Polygon[]): BBox {
   const allY = polygons.flatMap((polygon) => polygon.map((point) => point[1]));
   const allX = polygons.flatMap((polygon) => polygon.map((point) => point[0]));
-  const yMin = Math.min(...allY);
-  const yMax = Math.max(...allY);
-  const xMin = Math.min(...allX);
-  const xMax = Math.max(...allX);
-  return [xMin, yMin, xMax, yMax];
+  return new BBox(
+    Math.min(...allX),
+    Math.min(...allY),
+    Math.max(...allX),
+    Math.max(...allY),
+  );
 }
