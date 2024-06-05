6ace9aed3f151dbedab5d9df4008adb710568ea1
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2024-06-05 16:55:54 +0200

    fix polygon naming

--- src/imageOperations/common/imageExtractor.ts
+++ src/imageOperations/common/imageExtractor.ts
@@ -12,16 +12,16 @@ export async function extractFromPage(
   polygons: Polygon[]) {
   const { width, height } = pdfPage.getSize();
   const extractedElements :Uint8Array[] = [];
-  for (const boundingBox of polygons) {
+  for (const polygon of polygons) {
     const tempPdf = await PDFDocument.create();
 
-    const newWidth = width * (getMinMaxX(boundingBox).max - getMinMaxX(boundingBox).min);
-    const newHeight = height * (getMinMaxY(boundingBox).max - getMinMaxY(boundingBox).min);
+    const newWidth = width * (getMinMaxX(polygon).max - getMinMaxX(polygon).min);
+    const newHeight = height * (getMinMaxY(polygon).max - getMinMaxY(polygon).min);
     const cropped = await tempPdf.embedPage(pdfPage, {
-      left: getMinMaxX(boundingBox).min * width,
-      right: getMinMaxX(boundingBox).max * width,
-      top: height - (getMinMaxY(boundingBox).min * height),
-      bottom: height - (getMinMaxY(boundingBox).max * height),
+      left: getMinMaxX(polygon).min * width,
+      right: getMinMaxX(polygon).max * width,
+      top: height - (getMinMaxY(polygon).min * height),
+      bottom: height - (getMinMaxY(polygon).max * height),
     });
     const samplePage = tempPdf.addPage([newWidth, newHeight]);
     samplePage.drawPage(cropped,
