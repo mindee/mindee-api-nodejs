a5aa0f1ccaf3961883f42757144148f4e3b1bbb5
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2024-06-18 16:55:54 +0200

    :recycle: update company registration display

--- src/parsing/standard/companyRegistration.ts
+++ src/parsing/standard/companyRegistration.ts
@@ -1,5 +1,6 @@
 import { Field } from "./field";
 import { BaseFieldConstructor } from "./base"
+import { cleanOutString } from "../common";
 
 /**
  * A company registration item.
@@ -19,4 +20,19 @@ export class CompanyRegistrationField extends Field {
     super({ prediction, valueKey, reconstructed, pageId });
     this.type = prediction["type"];
   }
+  toTableLine(): string {
+    const printable = this.printableValues();
+    return `| ${printable["type"].padEnd(15)} | ${printable["value"].padEnd(20)} `;
+  }
+  toString(): string {
+    const printable = this.printableValues();
+    return `Type: ${printable["type"]}, Value: ${printable["value"]}`;
+  }
+  private printableValues(): { [key: string]: string } {
+    const printable: { [key: string]: string } = {};
+    printable["type"] = cleanOutString(this.type);
+    printable["value"] = cleanOutString((this.value ?? "").toString());
+    return printable;
+  }
+
 }
