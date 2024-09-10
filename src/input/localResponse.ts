7b3320a42cb14ae484009c66305844698ea152eb
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2024-09-10 17:39:59 +0200

    :recycle: add missing tests for localresponse (#293)

--- src/input/localResponse.ts
+++ src/input/localResponse.ts
@@ -6,6 +6,7 @@ import { Buffer } from "buffer";
 
 /**
  * Local response loaded from a file.
+ * Note: Has to be initialized through init() before use.
  */
 export class LocalResponse {
   private file: Buffer;
