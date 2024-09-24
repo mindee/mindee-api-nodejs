755487df610f955d04bbbad7b4e85a040c09bfcd
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2024-09-24 14:22:21 +0200

    :recycle: update integration tests & example scripts (#300)

--- example/invoiceSplitterTutorial.js
+++ example/invoiceSplitterTutorial.js
@@ -2,19 +2,20 @@ const { Client, product, imageOperations } = require("mindee");
 const { setTimeout } = require("node:timers/promises");
 
 async function parseInvoices() {
-    // fill in your API key or add it as an environment variable
-    const mindeeClient = new Client();
+  // fill in your API key or add it as an environment variable
+  const mindeeClient = new Client();
 
-    const invoiceFile = mindeeClient.docFromPath("path/to/your/file.ext");
-    const resp = await mindeeClient.enqueueAndParse(product.InvoiceSplitterV1, invoiceFile);
-    let invoices = await imageOperations.extractInvoices(invoiceFile, resp.document.inference);
-    for (const invoice of invoices) {
-        // optional: save the documents locally
-        invoice.saveToFile(`/tmp/invoice_p_${invoice.pageIdMin}-${invoice.pageIdMax}.pdf`);
-        const respInvoice = await mindeeClient.parse(product.InvoiceV4, invoice.asSource());
-        console.log(respInvoice.document.toString());
-        await setTimeout(1000); // wait some time between requests as to not overload the server
-    }
+  const invoiceFile = mindeeClient.docFromPath("path/to/your/file.ext");
+  const resp = await mindeeClient.enqueueAndParse(product.InvoiceSplitterV1, invoiceFile);
+  let invoices = await imageOperations.extractInvoices(invoiceFile, resp.document.inference);
+  for (const invoice of invoices) {
+    // optional: save the documents locally
+    invoice.saveToFile(`/tmp/invoice_p_${invoice.pageIdMin}-${invoice.pageIdMax}.pdf`);
+    const respInvoice = await mindeeClient.parse(product.InvoiceV4, invoice.asSource());
+    console.log(respInvoice.document.toString());
+    await setTimeout(1000); // wait some time between requests as to not overload the server
+  }
 
 }
+
 parseInvoices();
--- example/multiReceiptsTutorial.js
+++ example/multiReceiptsTutorial.js
@@ -16,4 +16,5 @@ async function parseReceipts() {
   }
 
 }
+
 parseReceipts();
