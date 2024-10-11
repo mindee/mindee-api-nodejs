e4fd3989c3dce027d2d5c42b44e112c117cc247e
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2024-10-11 12:43:37 +0200

    add unit test

--- tests/fields/date.spec.ts
+++ tests/fields/date.spec.ts
@@ -12,11 +12,13 @@ describe("Test Date field", () => {
         [0.414, 0.831],
         [0.016, 0.831],
       ],
+      is_computed: true
     };
     const field = new DateField({ prediction });
     expect(field.value).to.be.equal(prediction.value);
     expect(field.dateObject).to.be.deep.equal(new Date(prediction.value));
     expect(field.boundingBox).to.have.deep.members(prediction["polygon"]);
+    expect(field.isComputed).to.be.true;
   });
   it("Should create a Date field with N/A value as input", () => {
     const prediction = {
