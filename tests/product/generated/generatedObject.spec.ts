eebde08e4b79af34479b5b5caf731a47e6a13617
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2024-10-17 15:26:00 +0200

    tweak test

--- tests/product/generated/generatedObject.spec.ts
+++ tests/product/generated/generatedObject.spec.ts
@@ -3,10 +3,11 @@ import {GeneratedObjectField} from "../../../src/parsing/generated";
 
 describe("Generated Object Field", async () => {
   it("should properly format booleans.", async () => {
-    const dummyValue = { value: false, toto: true, hello: null };
+    const dummyValue = { value: false, toto: true, hello: "false", other: "true" };
     const genObjectField = new GeneratedObjectField({ prediction: dummyValue });
     expect(genObjectField.toto).to.be.true;
-    expect(genObjectField.hello).to.be.null;
+    expect(genObjectField.hello).to.be.equals("false");
+    expect(genObjectField.other).to.be.equals("true");
     expect(genObjectField.value).to.be.false;
   });
 });
