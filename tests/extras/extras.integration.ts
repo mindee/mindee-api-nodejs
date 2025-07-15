6fd398a57098ea594570d4fb815af97e52a653f5
Ianaré Sévi <ianare@mindee.co>
2025-07-15 17:55:34 +0200

    :white_check_mark: add mvision tests

--- tests/extras/extras.integration.ts
+++ tests/extras/extras.integration.ts
@@ -15,7 +15,9 @@ describe("Mindee Client Integration Tests", async () => {
       "tests/data/products/invoices/default_sample.jpg"
     );
     await sample.init();
-    const response = await client.parse(mindee.product.InvoiceV4, sample, { cropper: true });
+    const response = await client.parse(
+      mindee.product.InvoiceV4, sample, { cropper: true }
+    );
     expect(response.document.inference.pages[0]?.extras?.cropper).to.exist;
   }).timeout(60000);
 
@@ -24,8 +26,36 @@ describe("Mindee Client Integration Tests", async () => {
       "tests/data/products/international_id/default_sample.jpg"
     );
     await sample.init();
-    const response = await client.enqueueAndParse(mindee.product.InternationalIdV2, sample, { fullText: true });
+    const response = await client.enqueueAndParse(
+      mindee.product.InternationalIdV2, sample, { fullText: true }
+    );
     expect(response.document?.extras?.fullTextOcr).to.exist;
 
   }).timeout(60000);
+
+  it("should send OCR words synchronously", async () => {
+    const sample = client.docFromPath(
+      "tests/data/products/financial_document/default_sample.jpg"
+    );
+    await sample.init();
+    const response = await client.parse(
+      mindee.product.FinancialDocumentV1, sample, { allWords: true }
+    );
+    expect(response.document?.ocr).to.exist;
+    expect(response.document?.ocr?.toString()).to.not.be.empty;
+
+  }).timeout(65000);
+
+  it("should send OCR words asynchronously", async () => {
+    const sample = client.docFromPath(
+      "tests/data/products/financial_document/default_sample.jpg"
+    );
+    await sample.init();
+    const response = await client.enqueueAndParse(
+      mindee.product.FinancialDocumentV1, sample, { allWords: true }
+    );
+    expect(response.document?.ocr).to.exist;
+    expect(response.document?.ocr?.toString()).to.not.be.empty;
+
+  }).timeout(65000);
 });
