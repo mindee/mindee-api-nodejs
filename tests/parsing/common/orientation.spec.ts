d5c393330d3ba5be95099b35b0afbee978984a0f
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2025-07-17 15:48:04 +0200

    update CI trigger

--- tests/parsing/common/orientation.spec.ts
+++ tests/parsing/common/orientation.spec.ts
@@ -1,4 +1,4 @@
-import { OrientationField } from "../../src/parsing/common";
+import { OrientationField } from "../../../src/parsing/common";
 import { expect } from "chai";
 
 describe("Test Orientation field", () => {
