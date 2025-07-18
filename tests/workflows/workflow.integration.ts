ae1b705396a0759d890dfde0ea4fac9320e09560
ianaré <97107275+ianardee@users.noreply.github.com>
2025-07-18 12:13:37 +0200

    :white_check_mark: add mvision tests (#360)

--- tests/workflows/workflow.integration.ts
+++ tests/workflows/workflow.integration.ts
@@ -9,27 +9,60 @@ import { RAGExtra } from "../../src/parsing/common/extras/ragExtra";
 describe("Workflow calls", () => {
   let client: mindee.Client;
   let sample: LocalInputSource;
+  let workflowId: string;
+
   beforeEach(async () => {
     client = new mindee.Client();
+    workflowId = process.env["WORKFLOW_ID"] ?? "";
     sample = client.docFromPath(
       "tests/data/products/financial_document/default_sample.jpg"
     );
     await sample.init();
   });
+
   it("should retrieve a correct response from the API.", async () => {
     const currentDateTime = new Date().toISOString().replace(/T/, "-").replace(/\..+/, "");
     const response = await client.executeWorkflow(
       sample,
-      process.env["WORKFLOW_ID"] ?? "",
+      workflowId,
       { alias: `node-${currentDateTime}`, priority: ExecutionPriority.low, rag: true });
     expect(response.execution.priority).to.equal(ExecutionPriority.low);
     expect(response.execution.file.alias).to.equal(`node-${currentDateTime}`);
   }).timeout(60000);
 
-  it("should poll with rag enabled", async () => {
+  it("should poll with RAG disabled", async () => {
     const asyncParams: OptionalAsyncOptions = {
-      rag: true,
-      workflowId: process.env["WORKFLOW_ID"] ?? undefined
+      workflowId: workflowId
+    };
+    const response = await client.enqueueAndParse(
+      FinancialDocumentV1,
+      sample,
+      asyncParams
+    );
+    expect(response.document?.toString()).to.not.be.empty;
+    expect(response.document?.inference.extras?.rag).to.be.undefined;
+  }).timeout(60000);
+
+  it("should poll with RAG disabled and OCR words", async () => {
+    const asyncParams: OptionalAsyncOptions = {
+      workflowId: workflowId,
+      allWords: true
+    };
+    const response = await client.enqueueAndParse(
+      FinancialDocumentV1,
+      sample,
+      asyncParams
+    );
+    expect(response.document?.toString()).to.not.be.empty;
+    expect(response.document?.inference.extras?.rag).to.be.undefined;
+    expect(response.document?.ocr).to.exist;
+    expect(response.document?.ocr?.toString()).to.not.be.empty;
+  }).timeout(60000);
+
+  it("should poll with RAG enabled", async () => {
+    const asyncParams: OptionalAsyncOptions = {
+      workflowId: workflowId,
+      rag: true
     };
     const response = await client.enqueueAndParse(
       FinancialDocumentV1,
@@ -41,17 +74,20 @@ describe("Workflow calls", () => {
     expect(((response.document?.inference.extras?.rag) as RAGExtra).matchingDocumentId).to.not.be.empty;
   }).timeout(60000);
 
-  it("should poll with rag disabled", async () => {
+  it("should poll with RAG enabled and OCR words", async () => {
     const asyncParams: OptionalAsyncOptions = {
-      workflowId: process.env["WORKFLOW_ID"] ?? undefined
+      workflowId: workflowId,
+      rag: true,
+      allWords: true
     };
     const response = await client.enqueueAndParse(
       FinancialDocumentV1,
       sample,
       asyncParams
     );
-
     expect(response.document?.toString()).to.not.be.empty;
-    expect(response.document?.inference.extras?.rag).to.be.undefined;
+    expect(((response.document?.inference.extras?.rag) as RAGExtra).matchingDocumentId).to.not.be.empty;
+    expect(response.document?.ocr).to.exist;
+    expect(response.document?.ocr?.toString()).to.not.be.empty;
   }).timeout(60000);
 });
