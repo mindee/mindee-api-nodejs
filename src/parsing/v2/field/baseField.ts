d204ea32d2c4714e5c86058a714831094fe11268
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2025-08-04 12:33:50 +0200

    :recycle: remove unused DynamicField & put location data into BaseField

--- src/parsing/v2/field/baseField.ts
+++ src/parsing/v2/field/baseField.ts
@@ -1,14 +1,21 @@
 import { FieldConfidence } from "./fieldConfidence";
 import { StringDict } from "../../common";
+import { FieldLocation } from "./fieldLocation";
 
 export abstract class BaseField {
   protected _indentLevel: number;
   public confidence: FieldConfidence | undefined;
+  public locations: Array<FieldLocation> | undefined;
 
   protected constructor(rawResponse: StringDict, indentLevel = 0) {
     this._indentLevel = indentLevel;
-    if (rawResponse["confidence"] !== undefined) {
+    if ("confidence" in rawResponse && rawResponse["confidence"] !== null) {
       this.confidence = rawResponse["confidence"] as FieldConfidence;
+    }
+    if ("locations" in rawResponse && rawResponse["locations"]) {
+      this.locations = rawResponse["locations"].map((location: StringDict | undefined) => {
+        return location ? new FieldLocation(location) : "";
+      });
     }
   }
 }
