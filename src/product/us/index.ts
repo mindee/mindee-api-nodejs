b42bffd17df07292ec495f970527d9c550c5ce47
ianaré <97107275+ianardee@users.noreply.github.com>
2025-09-26 13:11:31 +0200

    :coffin: remove obsolete US Mail V2 (#389)

--- src/product/us/index.ts
+++ src/product/us/index.ts
@@ -1,4 +1,3 @@
 export { BankCheckV1 } from "./bankCheck/bankCheckV1";
 export { HealthcareCardV1 } from "./healthcareCard/healthcareCardV1";
-export { UsMailV2 } from "./usMail/usMailV2";
 export { UsMailV3 } from "./usMail/usMailV3";
--- src/product/us/usMail/internal.ts
+++ src/product/us/usMail/internal.ts
@@ -1,7 +1,3 @@
-export { UsMailV2 } from "./usMailV2";
-export { UsMailV2Document } from "./usMailV2Document";
-export { UsMailV2RecipientAddress } from "./usMailV2RecipientAddress";
-export { UsMailV2SenderAddress } from "./usMailV2SenderAddress";
 export { UsMailV3 } from "./usMailV3";
 export { UsMailV3Document } from "./usMailV3Document";
 export { UsMailV3RecipientAddress } from "./usMailV3RecipientAddress";
