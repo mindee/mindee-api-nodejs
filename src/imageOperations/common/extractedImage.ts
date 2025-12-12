bb7fb993996191d9ae71a7f4cf490e85516e1dee
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2025-12-12 18:14:28 +0100

    :sparkles: add multi-receipt custom file saving formats

--- src/imageOperations/common/extractedImage.ts
+++ src/imageOperations/common/extractedImage.ts
@@ -4,6 +4,9 @@ import { writeFileSync } from "node:fs";
 import path from "node:path";
 import { logger } from "../../logger";
 import { BufferInput } from "../../input";
+import { MIMETYPES } from "../../input/sources/localInputSource";
+import { Poppler } from "node-poppler";
+import { writeFile } from "fs/promises";
 
 /**
  * Generic class for image extraction
@@ -23,15 +26,68 @@ export class ExtractedImage {
    *
    * @param outputPath Path to save the file to.
    */
-  saveToFile(outputPath: string) {
+  async saveToFileAsync(outputPath: string) {
+    const fileExt = path.extname(outputPath).toLowerCase();
+    if (!MIMETYPES.has(fileExt)) {
+      throw new MindeeError(`Unsupported file extension: ${fileExt}`);
+    }
+
     try {
-      writeFileSync(path.resolve(outputPath), this.buffer);
+      let outputBuffer: Buffer = this.buffer;
+      if (fileExt !== ".pdf") {
+        const poppler = new Poppler();
+        const options: Record<string, unknown> = {
+          firstPageToConvert: 1,
+          lastPageToConvert: 1,
+          singleFile: true,
+        };
+
+        if (fileExt === ".png") {
+          options.pngFile = true;
+        } else if (fileExt === ".jpg" || fileExt === ".jpeg") {
+          options.jpegFile = true;
+        } else if (fileExt === ".tiff" || fileExt === ".tif") {
+          options.tiffFile = true;
+        }
+
+        const result = await poppler.pdfToCairo(this.buffer, undefined, options);
+        outputBuffer = Buffer.from(result, "latin1");
+      }
+
+      await writeFile(path.resolve(outputPath), outputBuffer);
       logger.info(`File saved successfully to ${path.resolve(outputPath)}.`);
     } catch (e) {
       if (e instanceof TypeError) {
         throw new MindeeError("Invalid path/filename provided.");
       } else {
         throw e;
+      }
+    }
+  }
+
+
+  /**
+   * Attempts to saves the document to a file synchronously.
+   * Throws an error if the file extension is not supported or if the file could not be saved to disk for some reason.
+   *
+   * @param outputPath Path to save the file to.
+   */
+  saveToFile(outputPath: string) {
+    const fileExt = path.extname(outputPath).toLowerCase();
+    if (fileExt !== ".pdf") {
+      throw new MindeeError(
+        `Unsupported file extension: ${fileExt}. For image formats, use saveToFileAsync() instead.`
+      );
+    } else {
+      try {
+        writeFileSync(path.resolve(outputPath), this.buffer);
+        logger.info(`File saved successfully to ${path.resolve(outputPath)}.`);
+      } catch (e) {
+        if (e instanceof TypeError) {
+          throw new MindeeError("Invalid path/filename provided.");
+        } else {
+          throw e;
+        }
       }
     }
   }
--- tests/v1/api/multiReceiptsReconstruction.spec.ts
+++ tests/v1/api/multiReceiptsReconstruction.spec.ts
@@ -1,24 +1,30 @@
 import { expect } from "chai";
 import { promises as fs } from "fs";
 import * as path from "path";
-import { Document } from "../../../src";
+import { Document, PathInput } from "../../../src";
 import { MultiReceiptsDetectorV1 } from "../../../src/product";
 import { extractReceipts } from "../../../src/imageOperations";
-import { PathInput } from "../../../src";
-import { V1_PRODUCT_PATH } from "../../index";
+import { RESOURCE_PATH, V1_PRODUCT_PATH } from "../../index";
 
 describe("MindeeV1 - A Multi-Receipt Document", () => {
-  it("should be split into the proper receipts", async () => {
+  let extractedReceipts: any[];
+  let sourceDoc: PathInput;
+  before(async () => {
+
     const jsonData = await fs.readFile(
       path.join(V1_PRODUCT_PATH, "multi_receipts_detector/response_v1/complete.json")
     );
-    const sourceDoc = new PathInput(
-      { inputPath: path.join(V1_PRODUCT_PATH, "multi_receipts_detector/default_sample.jpg") }
-    );
+
+    sourceDoc = new PathInput({
+      inputPath: path.join(V1_PRODUCT_PATH, "multi_receipts_detector/default_sample.jpg"),
+    });
     await sourceDoc.init();
+
     const response = JSON.parse(jsonData.toString());
     const doc = new Document(MultiReceiptsDetectorV1, response.document);
-    const extractedReceipts = await extractReceipts(sourceDoc, doc.inference);
+    extractedReceipts = await extractReceipts(sourceDoc, doc.inference);
+  });
+  it("should be split into the proper receipts", async () => {
     expect(extractedReceipts.length).to.be.equals(6);
     let i = 0;
     for (const extractedReceipt of extractedReceipts) {
@@ -27,6 +33,29 @@ describe("MindeeV1 - A Multi-Receipt Document", () => {
       expect(Buffer.byteLength(extractedReceipt.asSource().fileObject)).to.be.lessThan(10485760);
       expect(Buffer.byteLength(extractedReceipt.asSource().fileObject)).to.be.greaterThan(100000);
       i++;
+    }
+  });
+
+  it("should be saved locally", async () => {
+    let i = 0;
+    for (const extractedReceipt of extractedReceipts) {
+      extractedReceipt.saveToFile(path.join(RESOURCE_PATH, `output/extracted_receipt_${i}.pdf`));
+      await extractedReceipt.saveToFileAsync(path.join(RESOURCE_PATH, `output/extracted_receipt_${i}.png`));
+      await extractedReceipt.saveToFileAsync(path.join(RESOURCE_PATH, `output/extracted_receipt_${i}.jpg`));
+      const pdfStat = await fs.stat(path.join(RESOURCE_PATH, `output/extracted_receipt_${i}.pdf`));
+      expect(pdfStat.size).to.be.greaterThan(500000); // Arbitrary to assert noticeable discrepancies between OSs.
+      const jpgStat = await fs.stat(path.join(RESOURCE_PATH, `output/extracted_receipt_${i}.jpg`));
+      expect(jpgStat.size).to.be.greaterThan(40000);
+      const pngStat = await fs.stat(path.join(RESOURCE_PATH, `output/extracted_receipt_${i}.png`));
+      expect(pngStat.size).to.be.greaterThan(300000);
+      i++;
+    }
+  }).timeout(10000);
+  after(async () => {
+    for (let i = 0; i < extractedReceipts.length; i++) {
+      await fs.unlink(path.join(RESOURCE_PATH, `output/extracted_receipt_${i}.pdf`));
+      await fs.unlink(path.join(RESOURCE_PATH, `output/extracted_receipt_${i}.jpg`));
+      await fs.unlink(path.join(RESOURCE_PATH, `output/extracted_receipt_${i}.png`));
     }
   });
 });
