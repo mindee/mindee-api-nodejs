2235aa8de3302d04795bba3abaec59552f38f3c7
Ianaré Sévi <ianare@mindee.co>
2026-02-12 14:43:15 +0100

    :coffin: remove energy_bill_fra_v1_async, french_healthcard_v1_async, payslip_fra_v3_async

--- src/v1/product/fr/index.ts
+++ src/v1/product/fr/index.ts
@@ -1,14 +1,7 @@
 export { BankAccountDetailsV1 } from "./bankAccountDetails/index.js";
 export { BankAccountDetailsV2 } from "./bankAccountDetails/index.js";
-export { EnergyBillV1 } from "./energyBill/index.js";
-export { HealthCardV1 } from "./healthCard/index.js";
 export { IdCardV2 } from "./idCard/index.js";
-export { PayslipV3 } from "./payslip/index.js";
 export { CarteGriseV1 } from "./carteGrise/index.js";
 
-// not sure if we want to export these -- advanced users can import them directly
 export * as bankAccountDetails from "./bankAccountDetails/index.js";
-export * as energyBill from "./energyBill/index.js";
-export * as healthCard from "./healthCard/index.js";
 export * as idCard from "./idCard/index.js";
-export * as payslip from "./payslip/index.js";
