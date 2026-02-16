9e560e04c1ce2c35edecf4a07384e9553f9837fe
ianaré <97107275+ianardee@users.noreply.github.com>
2026-02-16 11:06:22 +0100

    :recycle: v1 ocr word should be a class (#437)

--- src/v1/parsing/standard/word.ts
+++ src/v1/parsing/standard/word.ts
@@ -1,11 +1,18 @@
-import * as geometry from "@/geometry/index.js";
+import { Polygon } from "@/geometry/index.js";
+import { StringDict } from "@/parsing/index.js";
 
-export type Word = {
+export class Word {
   /**
    * Contains the relative vertices coordinates (points) of a polygon containing
    * the field in the document.
    */
-  polygon: geometry.Polygon;
+  polygon: Polygon;
   text: string;
   confidence: number;
+
+  constructor(rawPrediction: StringDict) {
+    this.polygon = new Polygon(...rawPrediction["polygon"]);
+    this.text = rawPrediction["text"];
+    this.confidence = rawPrediction["confidence"];
+  }
 };
