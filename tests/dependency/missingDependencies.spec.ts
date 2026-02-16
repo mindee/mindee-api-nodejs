3c725509d5f1b6989725a5be378a2a8ecda1d51f
ianaré <97107275+ianardee@users.noreply.github.com>
2026-02-16 18:59:21 +0100

    :bug: fix for optional node-poppler (#439)

--- tests/dependency/missingDependencies.spec.ts
+++ tests/dependency/missingDependencies.spec.ts
@@ -3,38 +3,26 @@ import { describe, it } from "node:test";
 
 describe("MindeeV1 - Optional Dependencies #OptionalDepsRemoved", function () {
 
-  it("should NOT have sharp installed", async function () {
-    try {
-      const moduleName = "sharp";
-      await import(moduleName);
-      assert.fail("sharp should not be installed in this environment, but it was found!");
-    } catch (error: any) {
-      const isModuleNotFound = error.code === "ERR_MODULE_NOT_FOUND";
-      const isSharpBinaryMissing = error.message && error.message.includes("Could not load the \"sharp\" module");
+  const modules = [
+    "sharp",
+    "pdf.js-extract",
+    "@cantoo/pdf-lib",
+    "node-poppler",
+  ];
 
-      if (!isModuleNotFound && !isSharpBinaryMissing) {
-        throw error;
+  for (const moduleName of modules) {
+    it(`should NOT have ${moduleName} installed`, async function () {
+      try {
+        await import(moduleName);
+        assert.fail("sharp should not be installed in this environment, but it was found!");
+      } catch (error: any) {
+        const binaryMissing = error.message
+          && error.code === "ERR_MODULE_NOT_FOUND"
+          && error.message.includes(`Could not load the "${moduleName}" module`);
+        if (!binaryMissing) {
+          throw error;
+        }
       }
-    }
-  });
-
-  it("should NOT have pdf.js-extract installed", async function () {
-    try {
-      const moduleName = "pdf.js-extract";
-      await import(moduleName);
-      assert.fail("pdf.js-extract should not be installed, but it was found!");
-    } catch (error: any) {
-      assert.strictEqual(error.code, "ERR_MODULE_NOT_FOUND");
-    }
-  });
-
-  it("should NOT have @cantoo/pdf-lib installed", async function () {
-    try {
-      const moduleName = "@cantoo/pdf-lib";
-      await import(moduleName);
-      assert.fail("@cantoo/pdf-lib should not be installed, but it was found!");
-    } catch (error: any) {
-      assert.strictEqual(error.code, "ERR_MODULE_NOT_FOUND");
-    }
-  });
+    });
+  }
 });
